4a9aa4266b0948877e7097e66027827c45741224
Eldar Heleg <eldar.heleg@gmail.com>
2023-06-13 22:04:26 +0200

    dbConfig, env, indexjs

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+let secret = process.env.SECRET
+
+app.use(express.json());
+app.use(cors({origin: '*'}))
+
+
+// database conection
+const db = require('./models')
+
+
+// Routes
+const postsRoutes = require("./routes/Posts")
+app.use("/api/posts", postsRoutes);
+
+const authRoutes = require("./routes/Auth")
+app.use("/api/auth", authRoutes);
+
+
+
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("aplikacija pokrenuta");
+    })
+})
