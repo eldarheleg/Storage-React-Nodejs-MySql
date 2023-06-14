ff470098703ed78286b57918e23c254cccae2cd4
Eldar Heleg <eldar.heleg@gmail.com>
2023-06-14 21:01:16 +0200

    db config, models files init

--- server/index.js
+++ server/index.js
@@ -1,29 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-let secret = process.env.SECRET
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+let secret = process.env.SECRET;
 
 app.use(express.json());
-app.use(cors({origin: '*'}))
-
+app.use(cors({ origin: "*" }));
 
 // database conection
-const db = require('./models')
-
+const db = require("./models").default.default;
 
 // Routes
-const postsRoutes = require("./routes/Posts")
+const postsRoutes = require("./routes/Posts");
 app.use("/api/posts", postsRoutes);
 
-const authRoutes = require("./routes/Auth")
+const authRoutes = require("./routes/Auth");
 app.use("/api/auth", authRoutes);
 
-
-
-
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("aplikacija pokrenuta");
-    })
-})
+db.sequelize.sync({force: true}).then(() => {
+  app.listen(3001, () => {
+    console.log("aplikacija pokrenuta, drop db true");
+  });
+});
